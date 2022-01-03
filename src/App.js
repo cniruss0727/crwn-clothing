b24e96cfaf6be19ed5c6f1138d561e977a3ec525
cniruss0727 <cni@macalester.edu>
2022-01-02 22:25:56 -0600

    adding header component and links to home, shape page and contact

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import {Route, Switch} from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 function App() {
-  return <div>
+  return (
+  <div>
+    <Header />
     <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
     </Switch>
-  </div>;
+  </div>
+);
 }
 
 export default App;
